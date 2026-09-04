cb40f61cf12dd16bc2f2ca128711c9bdd60ba41c
Viktor Smirnov <tin.r@mail.ru>
2026-09-04 19:13:35 +0300

    updated category and product classes

--- src/category.py
+++ src/category.py
@@ -1,13 +1,29 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name: str, description: str, products: list):
+    def __init__(self, name: str, description: str, products: list) -> None:
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self) -> str:
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
--- src/product.py
+++ src/product.py
@@ -1,11 +1,48 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, product_params: dict, products_list=None) -> Product:
+        pp_name = product_params.get("name")
+        pp_description = product_params.get("description")
+        pp_price = product_params.get("price")
+        pp_quantity = product_params.get("quantity")
+
+        if isinstance(products_list, list):
+            for product in products_list:
+                if isinstance(product, Product):
+                    if pp_name == product.name:
+                        pp_quantity += product.quantity
+                        if pp_price < product.__price:
+                            pp_price = product.__price
+
+        return cls(pp_name, pp_description, pp_price, pp_quantity)
+
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+
+    @price.setter
+    def price(self, price: float) -> None:
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        if price < self.__price:
+            user_answer = input("Вы хотите понизить цену? y/N")
+            if user_answer.lower() == "y":
+                self.__price = price
+        else:
+            self.__price = price
+        return
